8ee8a8102c440c69e4bb112e44034045f7870b53
Paweł <kalicki.paul@icloud.com>
2022-01-13 21:50:42 +0100

    Added sample project

---
+++ app/main.py
@@ -0,0 +1,29 @@
+from fastapi import FastAPI, Request
+from fastapi.responses import HTMLResponse
+from fastapi.staticfiles import StaticFiles
+from fastapi.templating import Jinja2Templates
+
+from app.library.helpers import *
+from app.routers import unsplash, twoforms, accordion
+
+app = FastAPI()
+
+templates = Jinja2Templates(directory="templates")
+app.mount("/static", StaticFiles(directory="static"), name="static")
+
+app.include_router(unsplash.router)
+app.include_router(twoforms.router)
+app.include_router(accordion.router)
+
+
+
+@app.get("/", response_class=HTMLResponse)
+async def root(request: Request):
+    data = openfile("home.md")
+    return templates.TemplateResponse("page.html", {"request": request, "data": data})
+
+
+@app.get("/page/{page_name}", response_class=HTMLResponse)
+async def page(request: Request, page_name: str):
+    data = openfile(page_name+".md")
+    return templates.TemplateResponse("page.html", {"request": request, "data": data})
